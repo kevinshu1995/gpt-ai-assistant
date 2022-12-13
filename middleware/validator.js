2936ebd458350eedd5a8039677321b8b59f41e92
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-12-13 15:27:30 +0800

    [fix] remove validate header while develop

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,10 +1,13 @@
 import { validateSignature } from '../utils/index.js';
+import { APP_ENV } from '../config/index.js';
 
 const validator = secret => (req, res, next) => {
   const signature = req.header('x-line-signature');
-  if (secret && !validateSignature(req.rawBody, secret, signature)) {
-    res.sendStatus(403);
-    return;
+  if (APP_ENV === 'production') {
+    if (secret && !validateSignature(req.rawBody, secret, signature)) {
+      res.sendStatus(403);
+      return;
+    }
   }
   next();
 };
